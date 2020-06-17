ee5fbff52e3a156dd5905362ac38fe7f79b8361c
unknown <aannddrree>
2020-06-17 18:24:26 -0300

    Leitura de arquivo Spark exemplo

--- src/main/scala/br/com/projscala2/ApplicationIngestion.scala
+++ src/main/scala/br/com/projscala2/ApplicationIngestion.scala
@@ -1,7 +1,20 @@
 package br.com.projscala2
 
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+
 object ApplicationIngestion {
   def main(args: Array[String]): Unit = {
-    print("Ola")
+
+    val spark = SparkSession
+      .builder()
+      .appName("Scala Spark SQL basic example")
+      .config("spark.master", "local")
+      .getOrCreate();
+
+    val sc = spark.sparkContext
+    val textFile = sc.textFile("README.md")
+
+    print("teste: " + textFile.count())
   }
 }
